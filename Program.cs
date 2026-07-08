bb0f33be9bb370b7043cda83e99a65e3d67046aa
Mauro Silva <mauro.araujosilva21@gmail.com>
2026-07-07 23:12:05 -0300

    Fix: simplificando injecao do dbContext no EnsureCreated para corrigir build

--- Program.cs
+++ Program.cs
@@ -37,8 +37,7 @@ app.MapControllers();
 
 using (var scope = app.Services.CreateScope())
 {
-    var dbContext = scope.ServiceProvider.GetRequiredService<ControleGastos.Data.AppDbContext>();
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     dbContext.Database.EnsureCreated(); 
 }
-
 app.Run();
